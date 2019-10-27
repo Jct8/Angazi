4e9c63cfe5424ae32c0560dd4dd70990e0341e6e
Justin Tim <tim.justin8@gmail.com>
2019-10-27 00:28:39 +0000

    [Angazi] -Added Vector3 Signatures -Added Unit Tests
    git-tfs-id: [https://dev.azure.com/timjustin/]$/Angazi;C18

--- Math/Inc/EngineMath.h
+++ Math/Inc/EngineMath.h
@@ -3,3 +3,59 @@
 #include "Common.h"
 
 #include "Vector3.h"
+#include "Matrix4.h"
+
+namespace Angazi::Math
+{
+	constexpr float Dot(const Vector3& a, const Vector3& b)
+	{
+		return 0.0f;
+	}
+
+	constexpr float MagnitudeSqr(const Vector3& v)
+	{
+		return 0.0f; // homework
+	}
+
+	inline float Magnitude(const Vector3& v)
+	{
+		return sqrt(Magnitude(v));
+	}
+
+	inline Vector3 Normalize(const Vector3 & v)
+	{
+		return 0.0f;
+	}
+
+	constexpr Vector3 Cross(const Vector3 &a, const Vector3 &b)
+	{
+		return 0.0f;
+	}
+
+	constexpr Matrix4 Transpose(const Matrix4 &m)
+	{
+		return {};
+	}
+
+	constexpr Matrix4 Determinant(const Matrix4 &m)
+	{
+		return {};
+	}
+
+	constexpr Matrix4 Inverse(const Matrix4 &m)
+	{
+		return {};
+	}
+
+	constexpr Vector3 TransformCoord(const Vector3& v , const Matrix4 &m)
+	{
+		return {};
+	}
+	
+	constexpr Vector3 TransformNormal(const Vector3& v, const Matrix4 &m)
+	{
+		return {};
+	}
+
+
+}
--- Math/Inc/Vector3.h
+++ Math/Inc/Vector3.h
@@ -1,1 +1,37 @@
 #pragma once
+
+namespace Angazi::Math
+{
+	struct Vector3
+	{
+		float x, y, z;
+
+		constexpr Vector3() noexcept : Vector3{ 0.0f, } {} 
+		constexpr Vector3(float f) noexcept : Vector3{ f,f,f } {}
+		constexpr Vector3(float x, float y, float z) noexcept : x{ x }, y{ y }, z{z} {}
+
+		//Declaration
+		const static Vector3 Zero;
+		const static Vector3 One;
+		const static Vector3 XAxis;
+		const static Vector3 YAxis;
+		const static Vector3 ZAxis;
+
+		constexpr Vector3 operator-() const { return { -x,-y,-z }; }
+		constexpr Vector3 operator+(const Vector3 &v) const { return { x + v.x , y+ v.y ,z + v.z }; }
+		constexpr Vector3 operator-(const Vector3 &v) const { return { -x,-y,-z }; }
+		//constexpr Vector3 operator/() const { return { -x,-y,-z }; }
+
+		//Homework: add +,-,*,  and /
+
+		Vector3& operator+=(const Vector3& v) { x += v.x; y += v.y; z += v.z; return *this; }
+		Vector3& operator-=(const Vector3& v) { x -= v.x; y -= v.y; z -= v.z; return *this; }
+		Vector3& operator*=(const Vector3& v) { x *= v.x; y *= v.y; z *= v.z; return *this; }
+		Vector3& operator/=(const Vector3& v) { x /= v.x; y /= v.y; z /= v.z; return *this; }
+
+		//Homework: add -= , *= , /=
+
+
+	};
+	 
+}
