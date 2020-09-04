93227b0560934614f149551a5a8d5c05e24c747b
Justin Tim <tim.justin8@gmail.com>
2020-09-04 21:15:40 +0000

    - Removed graphic options in App.h  settings since changing graphics api is done through core - Removed OpenGL Projects as all projects can use both APIs now
    git-tfs-id: [https://dev.azure.com/timjustin/]$/Angazi;C358

--- OpenGLProjects/OpenGL_03_Cube/WinMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "GameState.h"
-#include <Angazi/Inc/Angazi.h>
-
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-{
-	Angazi::MainApp().AddState<GameState>("GameState");
-	Angazi::MainApp().Run({ "OpenGL Cube" ,1280, 720,Angazi::GraphicsAPI::OpenGL});
-	return 0;
-}
